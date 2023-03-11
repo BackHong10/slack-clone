8c69470257679f953c68491fc75125db35f6f792
abcd1234 <90763140+BackHong10@users.noreply.github.com>
2023-03-11 10:27:30 +0900

    3번째 클론 코딩

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,6 +6,9 @@ import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
 
 @Module({
   imports: [
@@ -14,6 +17,17 @@ import { UsersModule } from './users/users.module';
     ChannelsModule,
     WorkspacesModule,
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['entities/*.js'],
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
